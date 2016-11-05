f1128febda3075c59f54587069e621569366e57c
superoven <taylorritenour@berkeley.edu>
2016-11-05 16:34:19 -0700

    minor refactor and tweaks

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,12 +3,11 @@ extends Node2D
 var screen_size
 var player_size
 
-var MIN_SPEED = 50
+var MIN_SPEED = 10
 var MAX_SPEED = 220
 var ACCELERATION = 20
 
 var movement_speed = MIN_SPEED
-var direction = Vector2(0, 0)
 
 func _ready():
 	screen_size = get_viewport_rect().size
@@ -17,6 +16,7 @@ func _ready():
 
 func _get_normalized_direction():
 	var any_pressed = false
+	var direction = Vector2(0, 0)
 	if (Input.is_action_pressed("player_move_left")):
 		direction.x += -1
 		any_pressed = true
@@ -35,14 +35,17 @@ func _get_normalized_direction():
 	else:
 		return direction.normalized()
 
-func _process(delta):
-	var player = get_node('player_sprite')
-	var curr_pos = player.get_pos()
-	direction = self._get_normalized_direction()
+func _get_movement_vector(delta):
+	var direction = self._get_normalized_direction()
 	if movement_speed < MAX_SPEED:
 		movement_speed += ACCELERATION
 	else:
 		movement_speed = MAX_SPEED
-	var movement_vector = direction * delta * movement_speed
+	return direction * delta * movement_speed
+
+func _process(delta):
+	var player = get_node('player_sprite')
+	var curr_pos = player.get_pos()
+	var movement_vector = self._get_movement_vector(delta)
 	var new_pos = curr_pos + movement_vector
 	player.set_pos(new_pos)
